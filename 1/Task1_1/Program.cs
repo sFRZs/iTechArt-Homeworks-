f9dbe581cf53b98eb7eeab66e6f534ebed530ba4
sFRZs <78945749+sFRZs@users.noreply.github.com>
2021-10-11 17:55:32 +0300

    Add files via upload

---
+++ 1/Task1_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task1_1
+{
+    class Program1_1
+    {
+        static void Main(string[] args)
+        {
+            bool isCat = true;
+            byte weightOfChar = 2;
+            sbyte weightOfHamster = 50;
+            char symbolY = 'Y';
+            decimal dollarExchangeRate = 2.49m;
+            double pi = 3.1415;
+            float constAvogadro = 6.02E23f;
+            int speedOfLight = 300000000;
+            uint myAge = 19;
+            long numberOfBytesInMegabyte = 1048576;
+            ulong maxUlong = 18446744073709551615;
+            short numberOfPianoKeys = 88;
+            ushort numberOfCarWheels = 4;
+            object cat = "Cat";
+            string helloWorld = "Hello World!";
+        }
+    }
+}
---
+++ 1/Task1_2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Task1_2
+{
+    class Program1_2
+    {
+        static void Main(string[] args)
+        {
+            var a = 5;
+            var b = ++a;
+            var c = a + b * a--;
+            b = c != 50 ? 50 / a - 1 : c % 50;
+
+            bool answer = false;
+            bool answer2 = (b >= 60) ^ (c <= 48);
+
+            if ((b > 0 & c <= 50) || c % 2 == 0)
+            {
+                answer = true;
+
+            }
+
+            if ((c * c > 25) && answer)
+            {
+                Console.WriteLine(c * b);
+            }
+
+            if (Console.CapsLock | Console.NumberLock)
+            {
+                Console.WriteLine("On");
+            }
+
+
+
+            object d = b + 10;
+
+            if (d is int x)
+            {
+                Console.WriteLine(x + a - c);
+            }
+
+
+            string str = d as string;
+            Console.WriteLine(str);
+
+        }
+    }
+}
