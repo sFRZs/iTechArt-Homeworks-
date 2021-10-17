b5a6f97a166b2f9b16781d05d28229db80eeee3b
Тимофей Авдеев <78945749+sFRZs@users.noreply.github.com>
2021-10-17 19:45:51 +0300

    Add files via upload

---
+++ 2/Polymorphism/ClassA.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Polymorphism
+{
+    class ClassA : ParentClass
+    {
+        public void DisplayOverload(string a, int b)
+        {
+            Console.WriteLine($"DisplayOverload ClassA: {a} {b}");
+        }
+        public void Display()
+        {
+            Display2(ref _name, ref _name);
+            Console.WriteLine(_name);
+        }
+        private void Display2(ref string x, ref string y)
+        {
+            Console.WriteLine(_name);
+            x = "Akhil 1";
+            Console.WriteLine(_name);
+            y = "Akhil 2";
+            Console.WriteLine(_name);
+            _name = "Akhil 3";
+
+        }
+    }
+}
---
+++ 2/Polymorphism/ParentClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Polymorphism
+{
+    class ParentClass
+    {
+        protected string _name = "Akhil";
+
+
+        public void DisplayOverload(int a)
+        {
+            Console.WriteLine($"DisplayOverload int: {a}");
+        }
+        public void DisplayOverload(string a)
+        {
+            Console.WriteLine($"DisplayOverload string: {a}");
+        }
+
+        public void DisplayOverloadInt(int a, int b, int c)
+        {
+            Console.WriteLine($"DisplayOverloadIntnt: {a}, {b}, {c}");
+        }
+    }
+}
