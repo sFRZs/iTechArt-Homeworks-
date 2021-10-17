3eaee7e60737539da0ced6ce00e5b630e28b5c22
Тимофей Авдеев <78945749+sFRZs@users.noreply.github.com>
2021-10-17 19:34:00 +0300

    Add files via upload

---
+++ 2/Polymorphism/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var overload = new Override();
+            overload.DisplayOverload(28);
+            overload.DisplayOverload("Zlaya tarelka");
+            overload.DisplayOverloadInt(6, 129, 94);
+
+            var classA = new ClassA();
+            classA.DisplayOverload("Zlaya tarelka", 28);
+            classA.Display();
+
+            var classB = new ClassB();
+            classB.Display();
+            classB.DisplayInt();
+        }
+    }
+}
