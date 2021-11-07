9a951a5965b976944f98b3f0c66659f81df4e776
Timofey <studio28.frz@gmail.com>
2021-11-07 18:11:49 +0300

    Add files via upload

--- 4/Task1/Program.cs
+++ 4/Task1/Program.cs
@@ -7,24 +7,24 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            var plentyA = new Stack<int>();
-            var plentyB = new Stack<int>();
-            var plentyC = new Stack<int>();
+            var plentyA = new HashSet<int>();
+            var plentyB = new HashSet<int>();
+            var plentyC = new HashSet<int>();
 
             int[] arrayA = new[] {38, 418, 528, 956, 296, 540, 657, 575, 429, 361};
             int[] arrayB = new[] {570, 575, 266, 296, 793, 792, 409, 38, 480, 515};
 
             for (int i = 0; i < arrayA.Length; i++)
             {
-                plentyA.Push(arrayA[i]);
-                plentyB.Push(arrayB[i]);
+                plentyA.Add(arrayA[i]);
+                plentyB.Add(arrayB[i]);
             }
 
             foreach (var item in plentyA)
             {
                 if (plentyB.Contains(item))
                 {
-                    plentyC.Push(item);
+                    plentyC.Add(item);
                 }
             }
 
